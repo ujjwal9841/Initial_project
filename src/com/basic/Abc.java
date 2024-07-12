152c70165fb7ac5b55faf1dc1d861be32204798e
ujjwaltwanabasu <ujjwal.twanabasu@gmail.com>
2024-07-12 10:44:41 -0500

    created new java classes C and modified clas Abc

--- src/com/basic/Abc.java
+++ src/com/basic/Abc.java
@@ -3,8 +3,9 @@ package com.basic;
 public class Abc {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+      System.out.println("Hello");
+      
+     
 	}
 
 }
